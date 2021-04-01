a9ee2c52d6e8700ea62a0415bdfabbcb10d389c1
creeper121386 <694029828@qq.com>
2021-04-01 16:17:56 +0800

    [add] UNetLitModel

--- src/metrics.py
+++ src/metrics.py
@@ -3,7 +3,7 @@ from glob import glob
 
 import numpy as np
 from PIL import Image
-from globalenv import PSNR, SSIM
+from globalenv import PSNR, SSIM, console
 from util import calculate_psnr, calculate_ssim
 
 '''
@@ -20,31 +20,34 @@ assert len(folder1) == len(folder2)
 i = 0
 metrics = {
     PSNR: 0,
-    SSIM: 0
+    # SSIM: 0
 }
-# ipdb.set_trace()
+console.log(f'[*] Metrics: {list(metrics.keys())}')
+
 for x, y in zip(folder1, folder2):
     i += 1
-    print(f'[{i}] Calc: {x} and {y}')
+    console.log(f'[{i}] Now running: {x} and {y}')
     im1 = np.array(Image.open(x))
     im2 = np.array(Image.open(y))
 
     if im1.shape != im2.shape:
-        print(f'WARN: image shape mismatch: {im1.shape} != {im2.shape}. Resized.')
+        console.log(f'WARN: image shape mismatch: {im1.shape} != {im2.shape}. Resized.')
         im2.resize(tuple(im1.shape))
 
-    psnr = calculate_psnr(im1, im2)
-    ssim = calculate_ssim(im1, im2)
+    if PSNR in metrics:
+        psnr = calculate_psnr(im1, im2)
+        metrics[PSNR] += psnr
+        console.log(f'[{i}] PSNR: {psnr}, [AVG] PSNR: {metrics[PSNR] / i}')
 
-    # Use torch to compute, removed.
+    if SSIM in metrics:
+        ssim = calculate_ssim(im1, im2)
+        metrics[SSIM] += ssim
+        console.log(f'[{i}] SSIM: {ssim} - [AVG] SSIM: {metrics[SSIM] / i}')
+
+    # Use torch (GPU) to compute, removed.
     # im1 = torch.tensor(im1).unsqueeze(0)
     # im2 = torch.tensor(im2).unsqueeze(0)
     # psnr = ImageProcessing.compute_psnr(im1, im2, 255)
     # ssim = ImageProcessing.compute_ssim(im1, im2)
 
-    metrics[PSNR] += psnr
-    metrics[SSIM] += ssim
-
-    print(f'[{i}] PSNR: {psnr}, SSIM: {ssim} - [AVG] PSNR: {metrics[PSNR] / i}, SSIM: {metrics[SSIM] / i}')
-
-print(f'[AVG] PSNR: {metrics[PSNR] / i}, SSIM: {metrics[SSIM] / i}')
+# print(f'[AVG] PSNR: {metrics[PSNR] / i}, SSIM: {metrics[SSIM] / i}')
